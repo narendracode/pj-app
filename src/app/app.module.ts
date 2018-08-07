a24f7af3017d35c75ad27bb775c152c5d50895a9
Narendra <narendra.kumar.soni28@gmail.com>
2018-08-07 09:59:41 +0800

    improved UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,17 +14,25 @@ import { BrowserAnimationsModule }        from '@angular/platform-browser/animat
 import { MatToolbarModule }               from '@angular/material/toolbar';
 import { MatCardModule }                  from '@angular/material/card';
 import { MatSidenavModule }               from '@angular/material/sidenav';
-import { MatListModule}                   from '@angular/material/list';
-import { MatIconModule}                   from '@angular/material/icon';
-import { MatMenuModule}                   from '@angular/material/menu';
-import { MatTooltipModule}                from '@angular/material/tooltip';
-import { MatButtonModule}                 from '@angular/material/button';
+import { MatListModule }                   from '@angular/material/list';
+import { MatIconModule }                   from '@angular/material/icon';
+import { MatMenuModule }                   from '@angular/material/menu';
+import { MatTooltipModule }                from '@angular/material/tooltip';
+import { MatButtonModule }                 from '@angular/material/button';
 import { FlexLayoutModule }               from '@angular/flex-layout';
 import { MatFormFieldModule }             from '@angular/material/form-field';
 import { MatInputModule }                 from '@angular/material/input';
-
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { MatSelectModule }                from '@angular/material/select';
+import { MatCheckboxModule }               from '@angular/material/checkbox';
+import { MatRadioModule }                  from '@angular/material/radio';
+import { MatSliderModule }                 from '@angular/material/slider';
+import { MatDatepickerModule }             from '@angular/material/datepicker';
+import { MatSlideToggleModule }            from '@angular/material/slide-toggle';
+import { MatDividerModule }                from '@angular/material/divider';
+import { MatExpansionModule }              from '@angular/material/expansion';
+import { MatAutocompleteModule }           from '@angular/material/autocomplete';
+import { HeaderComponent }                from './header/header.component';
+import { FooterComponent }                from './footer/footer.component';
 
 
 @NgModule({
@@ -47,6 +55,15 @@ import { FooterComponent } from './footer/footer.component';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSliderModule,
+    MatDatepickerModule,
+    MatSlideToggleModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatAutocompleteModule,
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
