7a5c962cba2f6cee41123c9f6bee92119d8ecb43
Narendra <narendra.kumar.soni28@gmail.com>
2018-08-10 17:30:50 +0800

    receipt styling improved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,28 +14,30 @@ import { BrowserAnimationsModule }        from '@angular/platform-browser/animat
 import { MatToolbarModule }               from '@angular/material/toolbar';
 import { MatCardModule }                  from '@angular/material/card';
 import { MatSidenavModule }               from '@angular/material/sidenav';
-import { MatListModule }                   from '@angular/material/list';
-import { MatIconModule }                   from '@angular/material/icon';
-import { MatMenuModule }                   from '@angular/material/menu';
-import { MatTooltipModule }                from '@angular/material/tooltip';
-import { MatButtonModule }                 from '@angular/material/button';
+import { MatListModule }                  from '@angular/material/list';
+import { MatIconModule }                  from '@angular/material/icon';
+import { MatMenuModule }                  from '@angular/material/menu';
+import { MatTooltipModule }               from '@angular/material/tooltip';
+import { MatButtonModule }                from '@angular/material/button';
 import { FlexLayoutModule }               from '@angular/flex-layout';
 import { MatFormFieldModule }             from '@angular/material/form-field';
 import { MatInputModule }                 from '@angular/material/input';
 import { MatSelectModule }                from '@angular/material/select';
-import { MatCheckboxModule }               from '@angular/material/checkbox';
-import { MatRadioModule }                  from '@angular/material/radio';
-import { MatSliderModule }                 from '@angular/material/slider';
-import { MatDatepickerModule }             from '@angular/material/datepicker';
-import { MatSlideToggleModule }            from '@angular/material/slide-toggle';
-import { MatDividerModule }                from '@angular/material/divider';
-import { MatExpansionModule }              from '@angular/material/expansion';
-import { MatAutocompleteModule }           from '@angular/material/autocomplete';
+import { MatCheckboxModule }              from '@angular/material/checkbox';
+import { MatRadioModule }                 from '@angular/material/radio';
+import { MatSliderModule }                from '@angular/material/slider';
+import { MatDatepickerModule }            from '@angular/material/datepicker';
+import { MatSlideToggleModule }           from '@angular/material/slide-toggle';
+import { MatDividerModule }               from '@angular/material/divider';
+import { MatExpansionModule }             from '@angular/material/expansion';
+import { MatAutocompleteModule }          from '@angular/material/autocomplete';
+import { MatTableModule}                  from '@angular/material/table';
+
 import { HeaderComponent }                from './header/header.component';
 import { FooterComponent }                from './footer/footer.component';
-import { BillNewComponent } from './bill-new/bill-new.component';
-import { BillEditComponent } from './bill-edit/bill-edit.component';
-import { ReceiptComponent } from './receipt/receipt.component';
+import { BillNewComponent }               from './bill-new/bill-new.component';
+import { BillEditComponent }              from './bill-edit/bill-edit.component';
+import { ReceiptComponent }               from './receipt/receipt.component';
 
 
 
@@ -68,7 +70,7 @@ import { ReceiptComponent } from './receipt/receipt.component';
     MatDividerModule,
     MatExpansionModule,
     MatAutocompleteModule,
-    
+    MatTableModule,
 
 
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
--- src/app/receipt/receipt.component.ts
+++ src/app/receipt/receipt.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import printJS from 'print-js'
-
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-receipt',
@@ -8,20 +8,28 @@ import printJS from 'print-js'
   styleUrls: ['./receipt.component.scss']
 })
 export class ReceiptComponent implements OnInit {
-
-  constructor() { }
+  today =  new Date();
+  
+  constructor(
+  	private location: Location
+  ) { }
 
   ngOnInit() {
   }
 
   print(){
-  	console.log('print button is pressed..');
-
     printJS({ 
               type: 'html', 
               printable : 'receipt-container',
-              documentTitle: 'Receipt'
+              documentTitle: 'Receipt',
+              scanStyles: true,
+              style : '#receipt-container{border-color:#000}.jewellery-thumbnail{height:70px;margin-top:34px;border:none}.invoice,.invoice-header{margin-top:5px;text-align:center}.jewellery-thumbnail.jewellery-thumbnail-left{margin-left:30px}.jewellery-thumbnail.jewellery-thumbnail-right{//margin-left:30px}.invoice-header{font-family:American Typewriter,serif}.address,.buyer,.date,.invoiceId,.shop-address{font-family:monospace}.invoice-header .header1{font-size:3em}.invoice,.invoice-header .header2{font-size:1.5em}.invoice{text-color:#fff;background-color:grey;float:right}.address{font-size:1em}.buyer .buyer-header,.shop-address{font-size:1.5em}.buyer .buyer-name,.date,.invoiceId{font-size:1em}.table-header{font-family:Helvetica,sans-serif;font-size:.8em;text-align:center}.table-content{font-family:monospace;font-size:1em}.invoice-item-title{font-family:Helvetica,sans-serif;font-size:1.2em}.invoice-item-desc{font-family:monospace;font-size:1em}.signature-left{float:left}.signature-right{float:right}.signature-text{float:clear}.signature-text.signature-text-left{float:left}.signature-text.signature-text-right{float:right}.table td,.table th{text-align:left}' 
             });
+  }
+
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
