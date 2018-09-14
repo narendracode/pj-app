e7440b3eddf77b3ba9acdf527578b04fae970cbb
Narendra <narendra.kumar.soni28@gmail.com>
2018-09-14 21:10:30 +0800

    validation added

--- src/app/buyer-new/buyer-new.component.ts
+++ src/app/buyer-new/buyer-new.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Buyer } from '../buyer';
 import { BuyerService } from '../buyer.service';
-import { FormBuilder, FormGroup , FormControl, Validators,NgForm,FormsModule} from '@angular/forms';
+import { FormBuilder, FormGroup , FormControl, Validators,NgForm,FormsModule,FormGroupDirective} from '@angular/forms';
 import { patternValidator } from '../pattern-validator';
 
 @Component({
@@ -11,21 +10,79 @@ import { patternValidator } from '../pattern-validator';
   styleUrls: ['./buyer-new.component.scss']
 })
 export class BuyerNewComponent implements OnInit {
+  model = new Buyer('','','','','','','');
+  newBuyerRegisterForm: FormGroup;
+  @ViewChild(FormGroupDirective) myForm;
+
   constructor(private buyerService: BuyerService, private fb: FormBuilder) {
     // To initialize FormGroup  
     this.newBuyerRegisterForm = this.fb.group({   
-      'firstName':new FormControl('', Validators.compose([
+      'firstName': new FormControl('', Validators.compose([
          Validators.required
-      ])) 
-    }); 
+      ])),
+      'email': new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      'middleName' : new FormControl('', Validators.compose([
+      ])),
+      'lastName'   : new FormControl('', Validators.compose([
+         Validators.required
+      ])),
+      'company'    : new FormControl('', Validators.compose([
+      ])),
+      'address'    : new FormControl('', Validators.compose([
+         Validators.required
+      ])),
+      'phone'      : new FormControl('', Validators.compose([
+         Validators.required
+      ]))
+     }); 
+
+  /*this.newBuyerRegisterForm.setValue({
+    'firstName' : this.model.firstName,
+    'email'     : this.model.email,
+    'middleName': this.model.middleName,
+    'lastName'  : this.model.lastName,
+    'company'   : this.model.company,
+    'address'   : this.model.address,
+    'phone'     : this.model.phone
+  });*/
+
+
+  this.newBuyerRegisterForm.controls['firstName' ].setValue('');
+  this.newBuyerRegisterForm.controls['email' ].setValue('');
+  this.newBuyerRegisterForm.controls['middleName' ].setValue('');
+  this.newBuyerRegisterForm.controls['lastName' ].setValue('');
+  this.newBuyerRegisterForm.controls['company' ].setValue('');
+  this.newBuyerRegisterForm.controls['address' ].setValue('');
+  this.newBuyerRegisterForm.controls['phone' ].setValue('');
 
   }
-  model = new Buyer('','','','','','','');
-  newBuyerRegisterForm: FormGroup;
+
 
   validation_messages = {
     firstName : [
       { type: 'required', message: 'First name is required' }
+    ],
+    email : [
+      { type: 'required', message: 'Email is required' },
+      { type: 'pattern', message: 'Enter a valid email' }
+    ],
+    middleName : [
+      
+    ],
+    lastName : [
+      { type: 'required', message: 'Last name is required' }
+    ],
+    company : [
+      
+    ],
+    address : [
+      { type: 'required', message: 'Address is required' }
+    ],
+    phone : [
+      { type: 'required', message: 'Phone number is required' }
     ]
   };
 
@@ -33,17 +90,25 @@ export class BuyerNewComponent implements OnInit {
    
   }
 
-  onSubmit() { 
-    this.buyerService.addBuyer(this.model)
+  onSubmit(formDirective: NgForm) { 
+     this.model = new Buyer(this.newBuyerRegisterForm.value.firstName,
+                  this.newBuyerRegisterForm.value.middleName,
+                  this.newBuyerRegisterForm.value.lastName,
+                  this.newBuyerRegisterForm.value.company,
+                  this.newBuyerRegisterForm.value.address,
+                  this.newBuyerRegisterForm.value.phone,
+                  this.newBuyerRegisterForm.value.email
+      );
+      
+      this.buyerService.addBuyer(this.model)
       .subscribe(buyer => {
-         console.log('new Buyer added... ' + JSON.stringify(buyer));
          this.reset();
       });
+     
   }//onSubmit
   
   reset(){
-  	console.log('buyer reset is called...');
-  	this.model = new Buyer('','','','','','','');
+    this.newBuyerRegisterForm.reset();
   }
 
 }
--- src/app/buyer.ts
+++ src/app/buyer.ts
@@ -19,6 +19,14 @@ export class Buyer {
   address: string,
   phone: string,
   email: string
-  ){  }
+  ){
+      this.firstName = firstName;
+      this.middleName = middleName;
+      this.lastName = lastName;
+      this.company = company;
+      this.address = address;
+      this.phone = phone;
+      this.email = email;
+    }
 
 }
